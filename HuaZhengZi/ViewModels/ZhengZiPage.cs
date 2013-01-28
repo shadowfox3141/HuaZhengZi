e3f8fe2e43f2ea76194ce7c26f72e9d6f0dd50d5
Jimmy Ma <mayt444444@gmail.com>
2013-01-28 23:55:25 +0800

    0.0.0.1
    _____________________________
    not usable at all

---
+++ HuaZhengZi/ViewModels/ZhengZiPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace HuaZhengZi.ViewModels
+{
+    public class ZhengZiPage : INotifyPropertyChanged
+    {
+        public ZhengZiPage() {
+            _zhengZiCount = 0;
+        }
+
+        private string _pageName;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string PageName {
+            get {
+                return _pageName;
+            }
+            set {
+                if (value != _pageName) {
+                    _pageName = value;
+                    NotifyPropertyChanged("PageName");
+                }
+            }
+        }
+
+        private int _zhengZiCount;
+        public int ZhengZiCount {
+            get {
+                return _zhengZiCount;
+            }
+            set {
+                if (value != _zhengZiCount) {
+                    _zhengZiCount = value;
+                    NotifyPropertyChanged("ZhengZiCount");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName) {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler) {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ HuaZhengZi/ViewModels/ZhengZiPrensenter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using HuaZhengZi.Resources;
+
+namespace HuaZhengZi.ViewModels
+{
+    public class ZhengZiPrensenter : INotifyPropertyChanged
+    {
+        public ZhengZiPrensenter() {
+            this.Items = new ObservableCollection<ZhengZiPage>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ZhengZiPage> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty {
+            get {
+                return _sampleProperty;
+            }
+            set {
+                if (value != _sampleProperty) {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sample property that returns a localized string
+        /// </summary>
+        public string LocalizedSampleProperty {
+            get {
+                return AppResources.SampleProperty;
+            }
+        }
+
+        public bool IsDataLoaded {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData() {
+            // Sample data; replace with real data
+            
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName) {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler) {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
